40bba4c83ed928f95bc376b1041d689cc94abd52
Dave Anderson <nlclothing@gmail.com>
2020-12-03 00:28:06 -0500

    working changes

--- addon.py
+++ addon.py
@@ -2,42 +2,43 @@
 import os
 import sys
 import urllib
-import urllib2
-import cookielib
+import urllib.request
+import http.cookiejar
 import xbmcgui
 import xbmcaddon
 import simplejson as json
 
 __addon__       = xbmcaddon.Addon()
 __addonname__   = __addon__.getAddonInfo('name')
-__profile__ = xbmc.translatePath( __addon__.getAddonInfo('profile') ).decode("utf-8")
-__cwd__ = xbmc.translatePath( __addon__.getAddonInfo('path') ).decode("utf-8")
+__profile__ = xbmc.translatePath( __addon__.getAddonInfo('profile') )
+# __cwd__ = xbmc.translatePath( __addon__.getAddonInfo('path') )
 __icon__ = __addon__.getAddonInfo('icon')
+localize = __addon__.getLocalizedString
 
 from xbmcapi import XBMCSourcePlugin
 
 cookie_filename = __profile__+'pwg.cookie'
-cookieJar = cookielib.LWPCookieJar(cookie_filename)
+cookieJar = http.cookiejar.LWPCookieJar(cookie_filename)
 
 if os.access(cookie_filename, os.F_OK):
 	cookieJar.load(ignore_discard=True)
 
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookieJar))
-opener.add_handler(urllib2.HTTPSHandler())
+opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookieJar))
+opener.add_handler(urllib.request.HTTPSHandler())
 opener.addheaders = [('User-Agent', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.11) Gecko/20101012 Firefox/3.6.11'),]
 plugin = XBMCSourcePlugin()
 
 def checkMethods():
 	opts = [
-		{'dependancies': ['pwg.categories.getImages'], 'urivar': 'recent', 'labelid': 33100, 'adminonly': False},
-		{'dependancies': ['pwg.categories.getImages'], 'urivar': 'random', 'labelid': 33101, 'adminonly': False},
-		{'dependancies': ['pwg.categories.getImages'], 'urivar': 'rated', 'labelid': 33102, 'adminonly': False},
-		{'dependancies': ['pwg.categories.getList'], 'urivar': 'cats', 'labelid': 33103, 'adminonly': False},
-		{'dependancies': ['pwg.tags.getList', 'pwg.tags.getImages'], 'urivar': 'tags', 'labelid': 33104, 'adminonly': False},
-		{'dependancies': ['pwg.collections.getList', 'pwg.collections.getImages'], 'urivar': 'collection', 'labelid': 33119, 'adminonly': False},
-		{'dependancies': ['pwg.users.getFavorites'], 'urivar': 'favorites', 'labelid': 33120, 'adminonly': False},
-		{'dependancies': ['pwg.images.search'], 'urivar': 'search', 'labelid': 33105, 'adminonly': False},
-		{'dependancies': False, 'urivar': 'sync', 'labelid': 33114, 'adminonly': True},
+		{'dependancies': ['pwg.categories.getImages'], 'urivar': 'recent', 'labelid': 43100, 'adminonly': False},
+		{'dependancies': ['pwg.categories.getImages'], 'urivar': 'random', 'labelid': 43101, 'adminonly': False},
+		{'dependancies': ['pwg.categories.getImages'], 'urivar': 'rated', 'labelid': 43102, 'adminonly': False},
+		{'dependancies': ['pwg.categories.getList'], 'urivar': 'cats', 'labelid': 43103, 'adminonly': False},
+		{'dependancies': ['pwg.tags.getList', 'pwg.tags.getImages'], 'urivar': 'tags', 'labelid': 43104, 'adminonly': False},
+		{'dependancies': ['pwg.collections.getList', 'pwg.collections.getImages'], 'urivar': 'collection', 'labelid': 43119, 'adminonly': False},
+		{'dependancies': ['pwg.users.getFavorites'], 'urivar': 'favorites', 'labelid': 43120, 'adminonly': False},
+		{'dependancies': ['pwg.images.search'], 'urivar': 'search', 'labelid': 43105, 'adminonly': False},
+		{'dependancies': False, 'urivar': 'sync', 'labelid': 43114, 'adminonly': True},
 	]
 
 	returnopts = {}
@@ -61,14 +62,15 @@ def checkMethods():
 			addOpt = False
 
 		if addOpt == True:
-			returnopts[__addon__.getLocalizedString(opt['labelid'])] = opt['urivar']
+			# returnopts[localize(opt['labelid'])] = opt['urivar']
+			returnopts[opt['urivar']] = opt['urivar']
 
 	return returnopts
 
 def home():
 	opts = checkMethods()
 
-	for key in opts.iteritems():
+	for key in opts.items():
 		listitem = xbmcgui.ListItem(key[0])
 		plugin.addDirectoryItem(url='%s/%s' % (plugin.root, key[1]), listitem=listitem, isFolder=True)
 
@@ -81,8 +83,8 @@ def serverLogin():
 		'username' : plugin.getSetting('username'),
 		'password' : plugin.getSetting('password')
 	}
-	data = urllib.urlencode(values)
-	req = urllib2.Request(url, data)
+	data = urllib.parse.urlencode(values).encode('utf-8')
+	req = urllib.request.Request(url, data)
 	try:
 		conn = opener.open(req)
 		cookieJar.save(ignore_discard=True)
@@ -100,7 +102,7 @@ def serverLogin():
 				return True
 			else:
 				if(plugin.getSetting('username') != 'guest' and plugin.getSetting('username') != '') :
-					xbmcgui.Dialog().ok(__addonname__, __addon__.getLocalizedString(33106), __addon__.getLocalizedString(33107))
+					xbmcgui.Dialog().ok(__addonname__, localize(43106), localize(43107))
 					die(False)
 			pass
 
@@ -110,19 +112,19 @@ def serverRequest(method,extraData = []):
 		'method' : method
 	}
 	try:
-		for key in extraData.iteritems():
+		for key in extraData.items():
 			values[key[0]] = key[1]
 	except:
 		pass
-	data = urllib.urlencode(values)
-	req = urllib2.Request(url, data)
+	data = urllib.parse.urlencode(values).encode('utf-8')
+	req = urllib.request.Request(url, data)
 	conn = opener.open(req)
 	response = json.loads(conn.read())
 	conn.close()
 	if(response['stat'] == 'ok') :
 		return response['result']
 	else :
-		xbmcgui.Dialog().ok(__addonname__, __addon__.getLocalizedString(33109), '%s: %s' % (__addon__.getLocalizedString(33110), method), response['message'])
+		xbmcgui.Dialog().ok(__addonname__, localize(43109), '%s: %s' % (localize(43110), method), response['message'])
 		die(False)
 
 def populateDirectory(array):
@@ -132,7 +134,8 @@ def populateDirectory(array):
 		except:
 			listitem = xbmcgui.ListItem(obj['name'])
 		else:
-			listitem = xbmcgui.ListItem(obj['name'],iconImage=thumb)
+			listitem = xbmcgui.ListItem(obj['name'])
+			listitem.setArt(thumb)
 		finally:		
 			plugin.addDirectoryItem(url='%s/%s/%s/0' % (plugin.root, plugin.path, obj['id']), listitem=listitem, isFolder=True)
 	plugin.endOfDirectory()
@@ -146,7 +149,8 @@ def populateImages(imgs):
 				ttl = img['date_creation']
 		else :
 			ttl = img['name']
-		listitem = xbmcgui.ListItem(ttl,iconImage=img['derivatives']['thumb']['url'])
+		listitem = xbmcgui.ListItem(ttl)
+		listitem.setArt(img['derivatives']['thumb']['url'])
 		listitem.setInfo('pictures',{'date':img['date_available']})
 		commands = []
 		# commands.append(( 'Modify Tags', 'runnerAdd', ))
@@ -157,7 +161,7 @@ def populateImages(imgs):
 			thumb = img['derivatives']['xxlarge']['url']
 		plugin.addDirectoryItem(url=thumb, listitem=listitem)
 	if(int(plugin.getSetting('limit')) <= int(imgs['paging']['count'])) :
-		nextString = '> %s' % (__addon__.getLocalizedString(33115))
+		nextString = '> %s' % (localize(43115))
 		nextCount = plugin.getSetting('limit')
 		try:
 			imgCount = (imgs['paging']['page'] + 1)* imgs['paging']['per_page']
@@ -167,7 +171,7 @@ def populateImages(imgs):
 		except:
 			pass
 		try:
-			nextString += ' %s (%s %s)' % (nextCount, imgs['paging']['total_count'], __addon__.getLocalizedString(33116))
+			nextString += ' %s (%s %s)' % (nextCount, imgs['paging']['total_count'], localize(43116))
 		except:
 			nextString += ' %s' % (nextCount)
 		listitem = xbmcgui.ListItem(nextString)
@@ -206,7 +210,7 @@ def allCategories():
 
 def die(alert):
 	if alert:
-		xbmcgui.Dialog().ok(__addonname__, __addon__.getLocalizedString(33111), __addon__.getLocalizedString(33107))
+		xbmcgui.Dialog().ok(localize(43111), localize(43107))
 	raise SystemExit(0)	
 
 def syncServer():
@@ -220,20 +224,20 @@ def syncServer():
 		'subcats-included': 1,
 		'submit': 1
 	}
-	data = urllib.urlencode(values)
-	req = urllib2.Request(url, data)
+	data = urllib.parse.urlencode(values).encode('utf-8')
+	req = urllib.request.Request(url, data)
 	try:
 		conn = opener.open(req)
 	except:
-		xbmcgui.Dialog().ok(__addonname__, __addon__.getLocalizedString(33112), __addon__.getLocalizedString(33108))
+		xbmcgui.Dialog().ok(__addonname__, localize(43112), localize(43108))
 		die(False)
 	else:
 		response = conn.read()
 		conn.close()
 		if(response.find('scanning dirs')):
-			xbmc.executebuiltin('Notification(%s, %s, 5000, %s)'%(__addonname__, __addon__.getLocalizedString(33117), __icon__))
+			xbmc.executebuiltin('Notification(%s, %s, 5000, %s)'%(__addonname__, localize(43117), __icon__))
 		else:
-			xbmcgui.Dialog().ok(__addonname__, __addon__.getLocalizedString(33112), __addon__.getLocalizedString(33108))
+			xbmcgui.Dialog().ok(__addonname__, localize(43112), localize(43108))
 			die(False)
 
 if plugin.path:
@@ -285,10 +289,10 @@ if plugin.path:
 		populateImages(serverRequest('pwg.users.getFavorites', {'page':crntPage, 'per_page':plugin.getSetting('limit')}))
 	elif(split[0] == 'sync'):
 		syncServer()
-	elif(split[0] == 'views'):
-		xbmcgui.Window();
-		ui = modifyTags.modifyTagsWindow('piwigoTagDialog.xml' , __cwd__, 'Default')
-		ui.doModal()
+	# elif(split[0] == 'views'):
+	# 	xbmcgui.Window();
+	# 	ui = modifyTags.modifyTagsWindow('piwigoTagDialog.xml' , __cwd__, 'Default')
+	# 	ui.doModal()
 	elif(split[0] == 'search'):
 		try:
 			crntPage = int(split[2])
@@ -297,13 +301,13 @@ if plugin.path:
 		if(crntPage > 0):
 			populateImages(serverRequest('pwg.images.search', {'query':split[1], 'per_page':plugin.getSetting('limit'), 'page':crntPage}))
 		else:
-			keyboard = xbmc.Keyboard('', __addon__.getLocalizedString(33118), False)
+			keyboard = xbmc.Keyboard('', localize(43118), False)
 			keyboard.doModal()
 			if keyboard.isConfirmed() and keyboard.getText() != '':
 				plugin.path += '/%s' % (keyboard.getText())
 				populateImages(serverRequest('pwg.images.search', {'query':keyboard.getText(), 'per_page':plugin.getSetting('limit'), 'page':crntPage}))
 			else:
-				xbmcgui.Dialog().ok(__addonname__, __addon__.getLocalizedString(33113))
+				xbmcgui.Dialog().ok(__addonname__, localize(43113))
 	else :
 		home()
 else:
